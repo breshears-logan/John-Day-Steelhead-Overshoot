e15cc8a1b16b02308ebc34a84a77fe352891f7e3
ONID\bresheal <logan.breshears@oregonstate.edu>
2020-10-22 10:37:46 -0700

    Reset button

---
+++ R_Script_Header.R
@@ -0,0 +1,25 @@
+#Name: Logan Breshears
+#Agency:
+#Created on (Date):
+#Script contains code for:
+#1. (Title)
+
+
+
+
+## ~~~~~~~~~~~~~~~~...............~~~~~~~~~~~~~~~~~~~~~~~...........
+## ~~~~~~~~~~~~~~~~...............~~~~~~~~~~~~~~~~~~~~~~~...........
+#1.Start:  (Title)
+#Note: (Discription of project)
+
+## ~~~ set working directory and read in .CSV file ~~~ ##
+setwd("D:/Grad School/R-script")
+
+## ~~~ load packages ~~~ ##
+
+
+## ~~~ Statistical analysis ~~~ ##
+
+
+## ~~~ Create plots ~~~ ##
+#test (validation checks)
